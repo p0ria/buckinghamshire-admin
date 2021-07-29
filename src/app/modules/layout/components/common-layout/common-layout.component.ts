b85d5638d1355ce135dd3ca8a1b96397eb066b66
p0ria2 <poria.mast3r@gmail.com>
2021-07-29 20:51:21 +0430

    feat(sidebar): add sidebar-menu-items

--- src/app/modules/layout/components/common-layout/common-layout.component.ts
+++ src/app/modules/layout/components/common-layout/common-layout.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
+
+interface MenuItem {
+  title: string
+  icon: string
+  link: string
+}
 @Component({
   selector: 'bhs-common-layout',
   templateUrl: './common-layout.component.html',
@@ -8,6 +14,33 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class CommonLayoutComponent implements OnInit {
   isCollapsed = false;
+  menu: MenuItem[] = [
+    {
+      title: 'Dashboard',
+      icon: 'dashboard',
+      link: '/dashboard'
+    },
+    {
+      title: 'Escalations',
+      icon: 'exclamation-circle',
+      link: '/escalations'
+    },
+    {
+      title: 'PRO Carers',
+      icon: 'user',
+      link: '/carers'
+    },
+    {
+      title: 'Service Users',
+      icon: 'team',
+      link: '/patients'
+    },
+    {
+      title: 'Agencies',
+      icon: 'medicine-box',
+      link: '/agencies'
+    }
+  ]
 
   constructor() { }
 
