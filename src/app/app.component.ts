afaa8045f106701d4522bbe03f42899aa22a3c9a
p0ria2 <poria.mast3r@gmail.com>
2021-07-29 17:31:23 +0430

    feat(project): add project to git

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bhs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'admin';
+}
