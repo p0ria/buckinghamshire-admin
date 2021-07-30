08356279bf3f1c6d9784b4aae7b965812b031a3a
p0ria2 <poria.mast3r@gmail.com>
2021-07-30 15:39:40 +0430

    feat(login): add validation to sign-up form

--- src/app/modules/login/components/sign-in/sign-in.component.ts
+++ src/app/modules/login/components/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'bhs-sign-in',
@@ -7,10 +8,38 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SignInComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      rememberMe: [null]
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  submit = () => {
+    this.updateFormValueAndValidity();
+    if (this.form.valid) {
+
+    }
+  }
+
+  updateFormValueAndValidity = () => {
+    for (const key in this.form.controls) {
+      if (this.form.controls.hasOwnProperty(key)) {
+        this.form.controls[key].markAsDirty();
+        this.form.controls[key].updateValueAndValidity();
+      }
+    }
+  }
+
+  addEmailNotFoundError = () => {
+    this.form.get('email')!.setErrors({
+      notFound: true
+    })
   }
 
 }
